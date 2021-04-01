8a3b6260f78dffea4042d176f33c3557a08dc543
lekpramual <lek.pramual@gmail.com>
2021-04-01 15:53:56 +0700

    update location

--- src/pages/QRscanner.js
+++ src/pages/QRscanner.js
@@ -1,51 +1,78 @@
-import React, {useState} from 'react'
-import {Fab, TextareaAutosize} from '@material-ui/core'
-import {ArrowBack} from '@material-ui/icons'
+import React, { useState } from "react";
+import { Fab, TextareaAutosize } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
 import { Link } from "react-router-dom";
-import QrScan from 'react-qr-reader'
+import QrScan from "react-qr-reader";
+import { geolocated } from "react-geolocated";
 
-function QRscanner() {
-
-    const [qrscan, setQrscan] = useState('No result');
-    const handleScan = data => {
-        if (data) {
-            setQrscan(data)
-        }
+function QRscanner(props) {
+  const [qrscan, setQrscan] = useState("No result");
+  const handleScan = (data) => {
+    if (data) {
+      setQrscan(data);
     }
-    const handleError = err => {
-    console.error(err)
-    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
 
-    return (
-      <div>
-            <Link to="/">
-            <Fab style={{marginRight:10}} color="primary">
-                <ArrowBack/>
-            </Fab>
-            </Link>
-            <span>QR Scanner</span>
-            
-            <center>
-            <div style={{marginTop:30}}>
-                <QrScan
-                    delay={300}
-                    onError={handleError}
-                    onScan={handleScan}
-                    style={{ height: 240, width: 320 }}
-                />
-            </div>
-            </center>
+  return !props.isGeolocationAvailable ? (
+    <div>Your browser does not support Geolocation</div>
+  ) : !props.isGeolocationEnabled ? (
+    <div>
+      Geolocation is not enabled {JSON.stringify(props.isGeolocationAvailable)}
+    </div>
+  ) : props.coords ? (
+    <div>
+      <Link to="/">
+        <Fab style={{ marginRight: 10 }} color="primary">
+          <ArrowBack />
+        </Fab>
+      </Link>
+      <span>QR Scanner</span>
+      <p>
+        <span>latitude : {props.coords.latitude}</span>
+      </p>
+      <p>
+        <span>longitude : {props.coords.longitude}</span>
+      </p>
 
-            <TextareaAutosize
-                style={{fontSize:18, width:320, height:100, marginTop:100}}
-                rowsMax={4}
-                defaultValue={qrscan}
-                value={qrscan}
-            />
+      <center>
+        <div style={{ marginTop: 30 }}>
+          <QrScan
+            delay={300}
+            onError={handleError}
+            onScan={handleScan}
+            style={{ height: 240, width: 320 }}
+          />
+        </div>
+      </center>
 
-      </div>
-    );
-  }
-  
-  export default QRscanner;
-  
+      <TextareaAutosize
+        style={{ fontSize: 18, width: 320, height: 100, marginTop: 100 }}
+        rowsMax={4}
+        defaultValue={qrscan}
+        value={qrscan}
+      />
+    </div>
+  ) : (
+    <div>Getting the location data&hellip; </div>
+  );
+}
+
+export default geolocated({
+  // positionOptions: {
+  //   enableHighAccuracy: false
+  // },
+  positionOptions: {
+    enableHighAccuracy: true,
+    maximumAge: 0,
+    timeout: Infinity
+  },
+
+  watchPosition: false,
+  userDecisionTimeout: 5000,
+  suppressLocationOnMount: false,
+  geolocationProvider: navigator.geolocation,
+  isOptimisticGeolocationEnabled: true
+})(QRscanner);
